23d79b3d4940a34209b25f3ea08dfaa3c272f9a7
kilowat <kadantsev.sergey@gmail.com>
2019-10-15 13:04:28 +0300

    fix

--- resources/views/widgets/word_sort.blade.php
+++ resources/views/widgets/word_sort.blade.php
@@ -3,8 +3,8 @@
         <div class="sort-cell">
             <span class="sort-name">{{ $sort_item["name"] }}:</span>
             <span class="sort-arrow">
-                <a href="{{ $sort_item["desc"]["link"] }}" class="{{ $sort_item["desc"]["selected"] ? 'selected' : '' }}"><i class="material-icons dp48">arrow_downward</i></a>
-                <a href="{{ $sort_item["asc"]["link"] }}" class="{{ $sort_item["asc"]["selected"] ? 'selected' : '' }}"><i class="material-icons dp48">arrow_upward</i></a>
+                <a href="{{ $sort_item["asc"]["link"] }}" class="{{ $sort_item["asc"]["selected"] ? 'selected' : '' }}"><i class="material-icons dp48">arrow_downward</i></a>
+                <a href="{{ $sort_item["desc"]["link"] }}" class="{{ $sort_item["desc"]["selected"] ? 'selected' : '' }}"><i class="material-icons dp48">arrow_upward</i></a>
             </span>
         </div>
     @endforeach
