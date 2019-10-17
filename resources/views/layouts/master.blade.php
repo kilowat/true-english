1bc16c65c0af0a1901bee5f62a3f20f80fb22d5e
kilowat <kadantsev.sergey@gmail.com>
2019-10-17 11:58:39 +0300

    add yandex metrika

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="yandex-verification" content="726e7ffdbddea4cc" />
         <title>true-english.ru | @yield('title')</title>
         <meta name="description" content="@yield("description")" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -124,6 +123,21 @@
                 'border="0" width="88" height="120"><\/a>');
 
         </script>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(55809379, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/55809379" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
         @yield("js")
     </body>
 </html>
