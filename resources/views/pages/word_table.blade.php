f445df84aae4707b149971509f18137e2daf7996
kilowat <kadantsev.sergey@gmail.com>
2019-10-17 22:17:32 +0300

    some fix

--- resources/views/pages/word_table.blade.php
+++ resources/views/pages/word_table.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>true-english.ru | {{ $card->name }}</title>
+    <title>True-english - {{ $card->name }}</title>
     <meta name="description" content="{{ $card->description }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
