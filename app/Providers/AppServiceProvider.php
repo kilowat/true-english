8800f7caebe48135673ed6fa9e93d2ba33269bec
kilowat <kadantsev.sergey@gmail.com>
2019-10-27 20:55:32 +0300

    init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,10 +32,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('remote')) {
-            URL::forceSchema('https');
-        }
-
         Schema::defaultStringLength(191);
 
         WordCard::observe(WordCardObserver::class);
