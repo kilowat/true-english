0d5d711df11ccd88d8717887224f4a1e88095935
kilowat <kadantsev.sergey@gmail.com>
2019-11-24 22:25:39 +0300

    fix

--- resources/views/partials/word_info.blade.php
+++ resources/views/partials/word_info.blade.php
@@ -27,12 +27,12 @@
                     <div class="table-cell">Тренинг:</div>
                     <div class="table-cell">
                         <div class="link-list">
-                            @if($word->phrase_training == 0)
+                            @if($word->phrase_training > 0)
                                 <div class="link-row">
                                     <a href="{{ route('word-training.phrase', $word->name) }}" title="Фразы">Фразы ({{ $word->phrase_training }})</a>
                                 </div>
                             @endif
-                            @if($word->listen_training == 0)
+                            @if($word->listen_training > 0)
                                 <div class="link-row">
                                     <a href="{{ route('word-training.sentence', $word->name) }}" title="Фразы">Слух ({{ $word->listen_training }})</a>
                                 </div>
